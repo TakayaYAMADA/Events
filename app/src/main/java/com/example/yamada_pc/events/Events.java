d9afc97bda84a2e7e22579b9214e1d580567ef54
t-YAMADA <16043065@mmm.muroran-it.ac.jp>
2016-11-09 17:51:10 +0900

    complete ~P164

--- app/src/main/java/com/example/yamada_pc/events/Events.java
+++ app/src/main/java/com/example/yamada_pc/events/Events.java
@@ -6,15 +6,16 @@ import static android.provider.BaseColumns._ID;
 import static com.example.yamada_pc.events.Constants.TABLE_NAME;
 import static com.example.yamada_pc.events.Constants.TIME;
 import static com.example.yamada_pc.events.Constants.TITLE;
-import android.app.Activity;
+import android.app.ListActivity;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.StringBuilderPrinter;
+import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 
-public class Events extends Activity {
+public class Events extends ListActivity {
 
     private EventsData events;
 
@@ -53,19 +54,10 @@ public class Events extends Activity {
         return cursor;
     }
 
+    private static int[] TO = { R.id.rowid, R.id.time, R.id.title, };
     private void showEvents(Cursor cursor){
-        // カーソルの全ての内容を大きな文字列に詰め込む
-        StringBuilder builder = new StringBuilder("saved events:\n");
-        while (cursor.moveToNext()){
-            // インデックスの取得にはgetColumnsOrThrow()も使える
-            long id = cursor.getLong(0);
-            long time = cursor.getLong(1);
-            String title = cursor.getString(2);
-            builder.append(id).append(": ");
-            builder.append(time).append(": ");
-            builder.append(title).append("\n");
-        }
-        TextView text = (TextView) findViewById(R.id.text);
-        text.setText(builder);
+        // データバインドをセットアップする
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, R.layout.item, cursor, FROM, TO);
+        setListAdapter(adapter);
     }
 }
